989fd05627fda1578d85fd98826fedceb7d90272
vihanrs <vihanvrs@gmail.com>
2025-07-01 12:06:47 +0530

    initialize commit

---
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.pm.patientservice.repository;
+
+import com.pm.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
